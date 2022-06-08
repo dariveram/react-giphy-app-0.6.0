ce02917570f23f8907ac91ef646fa6412fedc198
David Rivera <drivera@ld.co.cr>
2022-06-07 20:58:33 -0600

    first commit

---
+++ docs/precache-manifest.7c3abbc251f64c3229b84e024f7c6010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd4c593b67fb993ecc33e0f9baf9870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b068751d8a8870e536d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b068751d8a8870e536d",
+    "url": "/static/js/main.f101a312.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
